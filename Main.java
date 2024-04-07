e36efec21db621706f1e579f457308cecacb7386
JuMill090 <julitamiller8@gmail.com>
2024-04-07 15:22:07 +0200

    zad1

--- Main.java
+++ Main.java
@@ -1,21 +1,9 @@
-import geometry.Circle;
-import geometry.Point;
-
 public class Main {
     public static void main(String[] args) {
-
-        //Środek koła
-        Point center = new Point(2.0, 2.0);
-
-        //
-        Circle circle = new Circle(center, 2.0);
-
-        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + circle.getRadius());
-        System.out.println("Obwód koła: " + circle.calculatePerimeter());
-        System.out.println("Pole powierzchni koła: " + circle.getArea());
-
-    
+        // Tworzymy obiekt klasy Point, zainicjuj go wartościami i wyświetl jego współrzędne
+        Point point = new Point(3.5, 2.0);
+        
+        System.out.println("Współrzędna x: " + point.getX());
+        System.out.println("Współrzędna y: " + point.getY());
     }
-    
 }
